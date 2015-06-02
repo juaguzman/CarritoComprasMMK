0168327735b16496319ffa428761df6bd0417c1f
juaguzman <juancarlos_891111@hotmail.com>
2015-06-02 02:50:13 -0500

    nina

--- Admin/header.php
+++ Admin/header.php
@@ -62,7 +62,13 @@ if(!isset($_SESSION["admins"]))
                 </ul>  
             </li>
             <li><a href="#"><span class="quinto"><i class="icon icon-text"></i></span><?php if (!isset($_SESSION["admins"])) {
-            echo "Invalido";}else {echo $_SESSION["admins"];} ?></a></li>           
+            echo "Invalido";}else {echo $_SESSION["admins"];} ?></a>
+                <ul>
+                                        <form  action="logout.php" method="post">
+                                        <li><input type="submit" name="submit" value="salir" class="bt_cssn" width="800px" /></li>
+                                        </form>
+               </ul>
+            </li>           
             </ul>
         </nav>
     </div>
