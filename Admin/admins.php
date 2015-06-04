9a2a9299628f34c613b8e5559a56f3726a26f47e
juaguzman <juancarlos_891111@hotmail.com>
2015-06-03 19:06:47 -0500

    casi final

---
+++ Admin/admins.php
@@ -0,0 +1,52 @@
+<?php 
+session_start();
+
+include '../conexion.php';
+include '../conex.php';
+        $mysql = new conexion();
+        $mysqli=$mysql->conctar();
+        
+$sql ="SELECT * FROM admin";
+$consulta=  mysql_query($sql);
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="css/estiloCategoria.css" type="text/css" media="screen" />
+        <title>Administrador de categorias</title>
+    </head>
+    <?php include 'header.php';?>
+    <body>
+        <h1>ADMINISTRADORES</h1>
+        <br>
+        <br>
+        <?php if(isset($_SESSION['admins']) && $_SESSION['admins']=='ROOT') { ?>
+        <table class="tg">
+  <tr>
+    <th class="tg-n0zm" colspan="6">Agregar Categoria</th>
+  </tr>
+  <tr>
+    <td class="tg-wy3v">Id admin</td>
+    <td class="tg-wy3v">Nombre</td>
+    <td class="tg-wy3v">Contraseña</td>
+    <td class="tg-wy3v">Email</td>
+    <td class="tg-wy3v" colspan="2">Acciones</td>
+  </tr>
+   <?php while($campos=mysql_fetch_object($consulta)){?>
+  <tr>
+    <td class="tg-031e"><?php echo $campos->idAdmin; ?></td>
+    <td class="tg-031e"><?php echo $campos->nombreAdmi; ?></td>
+    <td class="tg-031e"><?php echo $campos->contrasena; ?></td>
+    <td class="tg-031e"><?php echo $campos->contrasena; ?></td>
+    <td class="tg-031e"><a class="ac"href="#"  onclick="modificarCategoria(<?php echo $campos->idAdmin; ?>)"><img src="../img/modificar.png" width="30px" height="30px"/></a></td>
+    <td class="tg-031e"><a class="ac"href="#"  onclick="eliminarCategoria(<?php echo $campos->idAdmin; ?>)"><img src="../img/remove_event_256.png" width="30px" height="30px"/></a></td>
+  </tr>
+  <?php }?>
+</table>
+        <?php } else {?>
+        <h1>Solo el usuario ROOT tiene ingreso a esta pagina</h1>
+         <img id="inde" src="../img/error403.png">
+        <?php }?>
+    </body>
+</html>
---
+++ descripcion.php
@@ -0,0 +1,64 @@
+<?php
+        include './conexion.php';
+        $mysql = new conexion();
+        $mysqli=$mysql->conctar();
+      
+        
+if( isset($_GET['id']))
+{
+    $id = $_GET['id'];
+    $consulta= $mysqli->query("SELECT * FROM productos where idProducto=".$id);
+    $idc=$_GET['idc'];
+}
+
+?>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link rel="stylesheet" href="css/styleDesc.css" type="text/css" media="screen" />
+    </head>
+     <?php include './header.php';?>
+    <body>
+        <?php
+        // put your code here
+        ?> <h1>Producto</h1>
+        <br>
+        <br>
+        <div id="prod">
+        <table class="tbl">
+            <tbody>
+        <?php while($campos=mysqli_fetch_object($consulta)){?>
+            
+                <tr> <td colspan="2" class="tit"> <?php echo $campos->nombre; ?></td> </tr>
+        <tr> <td colspan="2"> <img src="imgProductos/<?php echo $campos->foto;?>" </td></tr>
+         <tr><td class="nom">Descripcion: </td> <td class="info"><?php echo $campos->descripcion; ?></td></tr>
+         <tr><td class="nom">Cantidad disponible: </td> <td class="info"><?php echo $campos->cantidad; ?></td></tr>
+         <tr><td class="nom">precio: </td> <td class="info"><?php echo $campos->presioVenta; ?></td></tr>
+         
+         <form name="form1" method="post" action="carrito.php">
+             <tr>  <td colspan="2" class="info"><input text-align="center"  name="cantidad" type="number" id="cantidad" value="1" height="50px"></td> </tr>
+             <tr><td colspan="2" class="info"> <input type="image" name="imageField" id="imageField" src="img/comprar.gif">
+              <input name="nombre" type="hidden" id="nombre" value="<?php echo $campos->nombre; ?>">
+              <input name="precio" type="hidden" id="precio" value="<?php echo $campos->presioVenta; ?>">
+              <input name="categoria" type="hidden" id="precio" value="<?php echo $campos->Categorias_idCat; ?>">
+              <input name="cantDisp" type="hidden" id="precio" value="<?php echo $campos->cantidad; ?>">
+              <input name="idprod" type="hidden" id="precio" value="<?php echo $campos->idProducto; ?>">
+              <input name="idcat" type="hidden" id="precio" value="<?php echo $idc; ?>">
+            </form> </tr>
+            <tr><td colspan="2" class="info"><a href="catalogo.php?id=<?php echo $idc;?>">Volver</a></td></tr>
+        
+           
+        <?php }?>
+        </tbody>
+        
+        </table>
+            </div>
+    </body>
+</html>
